bbbdb781023207e861e66d574f293a9c79aaebc0
Mark Lobodzinski <mark@lunarg.com>
2016-12-01 15:01:52 -0700

    tests: Remove layer doc check from run_all_tests
    Change-Id: I002179db5c24d5fee54d9999435a2b6fa1abb0d3

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -9,9 +9,6 @@ set -e
 #Verify that the loader is working
 ./run_loader_tests.sh
 
-# Verify that validation checks in source match documentation
-./vkvalidatelayerdoc.sh
-
 # vk_layer_validation_tests check to see that validation layers will
 # catch the errors that they are supposed to by intentionally doing things
 # that are wrong
